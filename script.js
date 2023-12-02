4a657cb0e8c12dd5f1cca5c342ab212b4dc76378
Ankiiisharma <myselfankit.2003@gmail.com>
2023-12-02 18:32:53 +0530

    cards adjusted

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const cardsE1 = document.querySelector(".cards");
 const cards = [];
+const imgs = ["batman", "hnm", "LV", "starbucks"];
 
-for(let i=0;i<8;i++){
+for (let i = 0; i < 8; i++) {
     let el = document.createElement("div");
     el.classList.add("card");
+    // Check the path to the images and ensure it is correct
+    el.innerHTML = `<img src="./images/${imgs[i % 4]}.png" alt="${imgs[i % 4]}"/>`;
     cards.push(el);
 }
 
-for(let c of cards){
+for (let c of cards) {
     cardsE1.append(c);
 }
